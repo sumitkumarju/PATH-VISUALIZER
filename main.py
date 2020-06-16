51bda60b61b468d630ba5a0d9bc5bd508deb9c13
Sumit Kumar <31334520+sumitkumarju@users.noreply.github.com>
2020-06-16 17:10:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -49,10 +49,10 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
         self.startnode={}
         self.endnode={}
         self.clear.pressed.connect(self.createGraphicView)
-        self.startnodeinput.toggled.connect(self.startselected)
-        self.endnodeinput.toggled.connect(self.endselected)
-        self.bfsinput.toggled.connect(self.bfs)
-        self.dfsinput.toggled.connect(self.print)
+        self.startnodeinput.pressed.connect(self.startselected)
+        self.endnodeinput.pressed.connect(self.endselected)
+        self.bfsinput.pressed.connect(self.bfs)
+        self.dfsinput.pressed.connect(self.dfs)
 
 
     def print(self):
@@ -67,6 +67,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
         for x in range(800,1300,25):
             for y in range(300,700,25):
                 self.scene.addRect(QRectF(x,y, 25, 25),borderColor,brush)
+                QApplication.processEvents()
                 time.sleep(0.1)
 
         #self.update()
@@ -77,6 +78,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
         #self.grayBrush = QBrush(Qt.gray)
 
         self.pen = QPen(Qt.red)
+
 
         #self.shapes()
                   # Grid pattern.
@@ -109,45 +111,63 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
 
 
 
-    def startselected(self, selected):
-        if selected:
+    def startselected(self):
             self.srt=True
 
-    def endselected(self, selected):
-        if selected:
+    def endselected(self):
             self.end=True
 
-    # def dfs_util(self,x,y,x_,y_):
-    #     if x==x_ and y==y_:
-    #         print("found")
-    #         return
+    def dfs_util(self, src: Point, dest: Point,visited):
 
-        # brush = QBrush()
-        # brush.setColor(Qt.white)
-        # brush.setStyle(Qt.SolidPattern)
-        # borderColor = Qt.black
-        # fillColor = Qt.red
-        # self.scene.addRect(QRectF(y*25,x*25, 25, 25),borderColor,brush)
-        # self.update()
-    #     time.sleep(0.1)
-    #     self.visited[x][y]=True
+        visited = [[False for i in range(COL)] for j in range(ROW)]
 
-    #     for i in range(4):
-    #         X=x+X_[i]
-    #         Y=y+Y_[i]
 
-    #         if X>=0 and X<33 and y>=0 and y<55 and self.visited[X][Y]==False and self.matrix[X][Y]!=1:
-    #             print(X,Y)
-    #             self.dfs_util(X,Y,x_,y_)
+        stack = []
 
-    # def dfs(self):
-        # x=int(self.startnode['y']/25)
-        # y=int(self.startnode['x']/25)
-        # x_=int(self.endnode['y']/25)
-        # y_=int(self.endnode['x']/25)
-    #     print("Initial ", x,y)
-    #     print("Required ", x_,y_)
-    #     self.dfs_util(x,y,x_,y_)
+
+        stack.append(src)
+        first = True
+        while (len(stack)):
+            # Pop a vertex from stack and print it
+            pt = stack[-1]
+            stack.pop()
+
+            # Stack may contain same vertex twice. So
+            # we need to print the popped item only
+            # if it is not visited.
+            if (not visited[pt.x][pt.y]):
+
+                visited[pt.x][pt.y] = True
+
+            if pt.x == dest.x and pt.y == dest.y:
+                return 0
+
+            if first == False:
+             self.paint(pt.y*25,pt.x*25,Qt.blue)
+             self.paint(pt.y*25,pt.x*25,Qt.white)
+
+            first=False
+
+            for i in range(4):
+                row = pt.x + rowNum[i]
+                col = pt.y + colNum[i]
+                if (isValid(row,col) and self.matrix[row][col] == 1 and not visited[row][col]):
+                    stack.append(Point(row,col))
+
+    def dfs(self):
+        x=int(self.startnode['y']/25)
+        y=int(self.startnode['x']/25)
+        x_=int(self.endnode['y']/25)
+        y_=int(self.endnode['x']/25)
+        source = Point(x,y)
+        dest = Point(x_,y_)
+        visited = [[False for i in range(COL)] for j in range(ROW)]
+        dist = self.dfs_util(source,dest,visited)
+
+        if dist!=-1:
+            print("Shortest Path is",dist)
+        else:
+            print("Shortest Path doesn't exist")
 
 
     def bfs_util(self, src: Point, dest: Point):
@@ -167,7 +187,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
 
         s = queueNode(src,0)
         q.append(s)
-
+        first = True
         while q:
 
             curr = q.popleft()
@@ -175,18 +195,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
             if pt.x == dest.x and pt.y == dest.y:
                 return curr.dist
 
-            self.paint(pt.y*25,pt.x*25)
+            if first == False:
+             self.paint(pt.y*25,pt.x*25,Qt.white)
 
-            #print({pt.y*25},{pt.x*25})
-            #self.x = f"QRectF({pt.y*25},{pt.x*25}, 25, 25)"
-            #self.update()
-            #QTimer.singleShot(2000, self.update)
+            first=False
+
             for i in range(4):
                 row = pt.x + rowNum[i]
                 col = pt.y + colNum[i]
                 if (isValid(row,col) and self.matrix[row][col] == 1 and not visited[row][col]):
                     visited[row][col] = True
                     Adjcell = queueNode(Point(row,col),curr.dist+1)
+                    if row == dest.x and col == dest.y:
+                       return curr.dist+1
+
+                    self.paint(col*25,row*25,Qt.blue)
+
                     q.append(Adjcell)
 
 
@@ -208,16 +232,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
             print("Shortest Path doesn't exist")
 
 
-    def paint(self,x,y):
-        print(x,y)
+    def paint(self,x,y,color):
+        #print(x,y)
         brush = QBrush()
-        brush.setColor(Qt.white)
+        brush.setColor(color)
         brush.setStyle(Qt.SolidPattern)
         borderColor = Qt.black
         fillColor = Qt.red
         self.scene.addRect(QRectF(x,y, 25, 25),borderColor,brush)
-        self.update()
-        time.sleep(0.001)
+        QApplication.processEvents()
+        time.sleep(0.01)
 
 
     def eventFilter(self, source, event):
@@ -233,9 +257,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
             if x<=1350 and y<=800: #25 50
                 self.matrix[int(y/25)][int(x/25)]=0
                 self.scene.addRect(QRectF(x,y, 25, 25),borderColor,brush)
-            #print('mouse move: (%d, %d)' % (pos.x(), pos.y()))
-            #print("X=",self.startnode)
-            #print("Y=",self.endnode)
+
         if (event.type() == QtCore.QEvent.MouseButtonPress and source is self.graphicsView.viewport() and event.button() == Qt.RightButton):
             pos = event.pos()
             brush = QBrush()
@@ -262,33 +284,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_Dialog):
 
 
 
-            #matrix[x][y]=1;
+
 
             self.scene.addRect(QRectF(x,y, 25, 25),borderColor,brush)
-            #print((self.matrix))
-            #print('mouse press: (%d, %d)' % (pos.x(), pos.y()))
+
         return QtWidgets.QWidget.eventFilter(self, source, event)
-
-
-    # def paintEvent(self, event):
-    #     #lastPoint = event.pos()
-    #     print(self.x)
-        # brush = QBrush()
-        # brush.setColor(Qt.white)
-        #     brush.setStyle(Qt.SolidPattern)
-        #     borderColor = Qt.black
-        #     fillColor = Qt.red
-        #     x=(self.lastPoint.x()-self.lastPoint.x()%25)
-        #     y=(self.lastPoint.y()-self.lastPoint.y()%25)
-        #     self.scene.addRect(QRectF(x-25,y-50, 25, 25),borderColor,brush)
-
-    def shapes(self):
-        ellipse = self.scene.addEllipse(20,20, 200,200, self.pen, self.greenBrush)
-        rect = self.scene.addRect(-100,-100, 200,200, self.pen, self.grayBrush)
-
-        ellipse.setFlag(QGraphicsItem.ItemIsMovable)
-        rect.setFlag(QGraphicsItem.ItemIsMovable)
-        ellipse.setFlag(QGraphicsItem.ItemIsSelectable)
 
 
 
